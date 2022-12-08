a586641c47d17a664478f32af4783448dfa11f01
Mahesh Purbia <maheshpurbia26@gmail.com>
2022-12-08 22:49:54 +0530

    Add files via upload

---
+++ SQL Query1.sql
@@ -0,0 +1,6 @@
+show databases;
+create database mahesh;
+use mahesh;
+create table student(name varchar(20), college varchar(20));
+insert into student values ('mahesh', 'IITbhu'),('Ravi', 'IITK');
+select * from student;
